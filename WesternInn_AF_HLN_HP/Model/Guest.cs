a0ea4f5c07628925b3a99e49b48b77ae3e485924
hanhzp <w.hanhle@gmail.com>
2022-09-19 15:24:06 +1000

    update guest.cs

--- WesternInn_AF_HLN_HP/Model/Guest.cs
+++ WesternInn_AF_HLN_HP/Model/Guest.cs
@@ -1,8 +1,13 @@
-﻿namespace WesternInn_AF_HLN_HP.Model
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace WesternInn_AF_HLN_HP.Model
 {
     public class Guest
     {
+        [Key]
         public string Email { get; set; }
+        
         public string Surname { get; set; }
         public string GivenName { get; set; }
         public string Postcode { get; set; }
