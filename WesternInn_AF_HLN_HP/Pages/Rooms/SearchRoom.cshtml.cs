a73b316ee19803c372226fc55b21bbfb4fef2cf4
Hiep Pham <19444084@student.westernsydney.edu.au>
2022-10-20 14:07:08 +1100

    everything updated

--- WesternInn_AF_HLN_HP/Pages/Rooms/SearchRoom.cshtml.cs
+++ WesternInn_AF_HLN_HP/Pages/Rooms/SearchRoom.cshtml.cs
@@ -1,12 +1,24 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace WesternInn_AF_HLN_HP.Pages.Rooms
 {
+    [Authorize(Roles="Guests")]
     public class SearchRoomModel : PageModel
     {
-        public void OnGet()
+        private readonly WesternInn_AF_HLN_HP.Data.ApplicationDbContext _context;
+
+        public CreateModel(WesternInn_AF_HLN_HP.Data.ApplicationDbContext context)
         {
+            _context = context;
         }
+        public IActionResult OnGet()
+        {
+            ViewData["RoomID"] = new SelectList(_context.Room, "ID", "Level");
+            return Page();
+        }
+
     }
 }
