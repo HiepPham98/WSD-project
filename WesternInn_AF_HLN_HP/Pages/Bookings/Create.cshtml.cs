7c6c3e54a02130cb6b7a7c3c96ea71ede9dd3570
Hiep Pham <19444084@student.westernsydney.edu.au>
2022-10-24 12:38:57 +1100

    Book a Room created

--- WesternInn_AF_HLN_HP/Pages/Bookings/Create.cshtml.cs
+++ WesternInn_AF_HLN_HP/Pages/Bookings/Create.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -10,6 +11,8 @@ using WesternInn_AF_HLN_HP.Model;
 
 namespace WesternInn_AF_HLN_HP.Pages.Bookings
 {
+    [Authorize(Roles="Guest")]
+    [Authorize(Roles ="Administrator")]
     public class CreateModel : PageModel
     {
         private readonly WesternInn_AF_HLN_HP.Data.ApplicationDbContext _context;
@@ -21,7 +24,7 @@ namespace WesternInn_AF_HLN_HP.Pages.Bookings
 
         public IActionResult OnGet()
         {
-        ViewData["RoomID"] = new SelectList(_context.Room, "ID", "Level");
+        ViewData["RoomID"] = new SelectList(_context.Room, "ID", "ID");
             return Page();
         }
 
@@ -32,10 +35,37 @@ namespace WesternInn_AF_HLN_HP.Pages.Bookings
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid)
+            if (!ModelState.IsValid || _context.Booking == null || Booking == null)
             {
                 return Page();
             }
+            var emptyBooking = new Booking();
+            /* emptyPurchase: the object to update using the values submitted.
+             * "Order": the Prefix used in the input names in web form.
+             * Lambda expression: list the properties to be updated. If not listed, 
+             *                    no updates, thus preventing overposting.
+             */
+            var success = await TryUpdateModelAsync<Booking>(emptyBooking, "Booking",
+                                s => s.RoomID, s => s.GuestEmail, s => s.CheckIn, s => s.CheckOut);
+           /* if (success)
+            {
+                var theBooking = await _context.Burger.FindAsync(emptyPurchase.BurgerID);
+                //calculate the total price of this oreder
+                emptyPurchase.TotalCost = emptyPurchase.BurgerCount * theBurger.Price;
+
+                // add this newly-created order into db
+                _context.Purchase.Add(emptyPurchase);
+
+                await _context.SaveChangesAsync();
+                if (purchase != null)
+                {
+                    //double totalCost = 0;
+                    ViewData["TotalCost"] = (double)emptyPurchase.TotalCost;
+
+                }
+                ViewData["BurgerName"] = theBurger.BurgerName;
+                return Page();
+            }*/
 
             _context.Booking.Add(Booking);
             await _context.SaveChangesAsync();
